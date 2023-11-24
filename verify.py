0023490f08889119b0f47d5e7c55dc984c0a1e7c
victorchan <victor2263@gmail.com>
2023-11-24 02:30:10 +0000

    Revert "Added standardised regular expressions to verify.py and implement in functions"
    This reverts commit fe9d8e97cec53cc2fbb6159ce98a2ae1fe2e5f25.

--- verify.py
+++ verify.py
@@ -105,7 +105,7 @@ def username(line):
         if _username == "0":
             return _username
         elif _username == "":
-            logging.error("User did not enter a username.")
+            logging.error('Entry is empty')
             print("Please enter a username.")
         elif not s:
             logging.error(f'Username {_username} input by user is not in the correct format.')
@@ -138,16 +138,14 @@ def phone_number(line):
         s = re.search("^\+?\d{1,3} \d{8,11}$", _number)  # allow starting + to be omitted
         if _number == "0":
             return _number
-        elif _number == "":
+        if _number == "":
             print("Please enter a phone number.")
-        elif _number[0] != "+":
+        if _number[0] != "+":
             _number = f"+{_number}"
-            return _number
-        elif not s:
+            break
+        if not s:
             logging.error(f'Phone number {_number} input by user is not in the correct format.')
             print("Incorrect phone number format. Please try again.")
-        else:
-            return _number
 
 
 
